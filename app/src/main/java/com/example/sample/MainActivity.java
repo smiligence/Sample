04b8a9893612c68fecc1b250fdd0b42917ac7e84
TestingSmiligence <pavithra.sarkunam@smiligence.com>
2022-07-21 11:10:41 +0530

    Initial Commit 1

--- app/src/main/java/com/example/sample/MainActivity.java
+++ app/src/main/java/com/example/sample/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //added comment
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
